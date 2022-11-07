5376b57e70161c3d4727bd321c0bd918f9ce2d14
87878787878799 <savelev.vitaliy2003@mail.ru>
2022-11-07 23:56:31 +0300

    test 2

--- main.py
+++ main.py
@@ -5,8 +5,22 @@ from bs4 import BeautifulSoup
 url = "https://www.meteoservice.ru/weather/hourly/lefortovo"
 pagebs = BeautifulSoup(requests.get(url).text, "lxml")
 today_waether = pagebs.find('div', class_="forecast-rows hourly-vertical")
-print(today_waether)
-for i in range(23):
-    gg = today_waether.find('div', onclick="$(this).toggleClass('expanded')")
-    print(gg)
-    break
+gg = today_waether.find_all('div', onclick="$(this).toggleClass('expanded')")
+print('bruh')
+
+# for i in range(23):
+#     gg = today_waether.find('div', onclick="$(this).toggleClass('expanded')")
+#     hh = gg.text
+#     print(hh)
+#     break
+
+
+
+# markers = ['облака', 'облаков', 'давление']
+# for i in range(len(hh)):
+#     if hh[i] in markers or :
+
+
+
+
+
